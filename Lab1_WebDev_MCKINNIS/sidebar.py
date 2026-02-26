24906020ad153d3b681b2e97c71eac1cd4be452a
abbymckinnis <107953512+abbymckinnis@users.noreply.github.com>
2026-02-26 18:09:39 -0500

    Delete Lab1_WebDev_MCKINNIS directory

--- Lab1_WebDev_MCKINNIS/sidebar.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def show_sidebar():
-    with st.sidebar:
-        st.header("Navigation")
-        st.page_link("Home_Page.py", label="Home")
-        st.page_link("pages/1_Portfolio.py", label="Portfolio")
-        # add more links as needed
