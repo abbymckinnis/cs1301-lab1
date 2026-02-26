f4ae2759d3c32b812bf65c30fce437089eeb2ebb
abbymckinnis <107953512+abbymckinnis@users.noreply.github.com>
2026-02-26 16:54:41 -0500

    Update info.py

--- Lab1_WebDev_MCKINNIS/info.py
+++ Lab1_WebDev_MCKINNIS/info.py
@@ -1,40 +1,76 @@
-# info.py
 
-# -------------------------
-# ABOUT ME
-# -------------------------
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
 profile_picture = "Images/gt.png"
+about_me = "I'm Abby McKinnnis. I am a CS Major at GA  Tech and I also play for the womens softball team!. "
 
-about_me = "Hi, I'm Abby. I'm a CS major at Georgia Tech."
 
-# -------------------------
-# EDUCATION
-# -------------------------
-education_data = {
-    "Field": ["Degree", "Institution", "Graduation"],
-    "Value": [
-        "BS Computer Science",
-        "Georgia Institute of Technology",
-        "2029"
-    ]
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/abigail-mckinnis-3603063ab/"
+my_github_url = "https://github.com/abbymckinnis"
+my_email_address = "amckinnis6@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2029',
+    'GPA': '4.0'
 }
-
-# -------------------------
-# COURSES
-# -------------------------
 course_data = {
-    "code": ["CS 1301"],
-    "names": ["Intro to Computing"],
-    "semester_taken": ["Spring 2026"],
-    "skills": ["Python basics"]
+    "code":["CS 1301", "CS 1300", "MATH 1551"], 
+    "names":["Intro to CS", "People Skills", "Calc 1"], 
+    "semester_taken":["2nd", "2nd", "1st"],
+    "skills":["Python is not my favorite snake anymore", "Connection between people and computers", "Number systems and values"],
+    }
+experience_data = {
+    "Coach at GAC": (["- Coached instead of playing an dpoured into others",
+                                                                          "- increased support from fans", "-Impressed and left a good mark on the school"],"Images/gac.png"),
+    "Board Member for BSAA committee":(["- Handle, promote, and communicate/support Black softball atheletes",
+                                                           "- improved racial tensions and black beleif"],"Images/bsa.png"),
+    "Head member of HS committee":(["- Managed and handled most Honors Society activities within the school in regards to tutoring and edu."],"Images/gt.png")
+
 }
 
-# -------------------------
-# EXPERIENCE
-# -------------------------
-experience_data = {
-    "Coach at GAC": (
-        ["Coached and supported student athletes"],
-        "Images/gac.png"
-    )
+projects_data = {
+    "RecruitHer App": "Helped develop a recruiting website to support girls in sports by improving visibility and connections.",
+    "Interactive Streamlit Quiz": "Built an interactive 5-question quiz using Streamlit with multiple question types, images, and dynamic results to match the user with a result type."
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Fluent except for some words",
+}
+leadership_data = {
+    "Coached at MSP All American Games": (["- Elected as one of the top coaches and earned my first win"],"Images/msp.png"),
+
+}
+activity_data={
+    "Minority Softball Prospects- All American Games": ["- Helped the foundation earn money by being the face of the program", 
+            "- Attracted so many viewers and participants."]
 }
