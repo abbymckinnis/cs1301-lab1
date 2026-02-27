a0543219992f6e9f26729d3377cac1806f5651db
abbymckinnis <107953512+abbymckinnis@users.noreply.github.com>
2026-02-26 20:22:18 -0500

    Add files via upload

---
+++ Lab1_WebDev_MCKINNIS/Home_Page.py
@@ -0,0 +1,29 @@
+import streamlit as st
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section X")
+st.subheader("Abby McKinnis")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. Portfolio/ About Me: On this main page is all of the functions used in order to complte the set up for my website design.An introduction to me as a person and some of my accomplishments.
+#       2. **Additonal Info*: It is the backboard behind all of the functions of the website, and contains the information that I imported about myself in order for it as well as the images to show on the website.
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. Portfolio/ About Me:
+
+
+2. Quiz Tab:
+
+""")
+
