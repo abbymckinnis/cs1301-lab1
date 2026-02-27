94db1c82b59551dfa03db22b128a55723c930d76
abbymckinnis <107953512+abbymckinnis@users.noreply.github.com>
2026-02-26 21:12:07 -0500

    Update Home_Page.py

--- Lab1_WebDev_MCKINNIS/Home_Page.py
+++ Lab1_WebDev_MCKINNIS/Home_Page.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="Lab 1 Home", page_icon="🏠", layout="centered")
+st.set_page_config(page_title="Lab 1 Home", page_icon="🏠")
 
-st.title("Web Development Lab 1")
-st.subheader("CS 1301")
-st.write("**Abby McKinnis**")
+st.title("CS 1301")
+st.subheader("Abby McKinnis")
 
 st.write("""
 Welcome to my Lab 1 Streamlit app! Use the sidebar to navigate between pages.
